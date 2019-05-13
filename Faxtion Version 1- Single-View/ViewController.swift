d26b60fecd0c010da133edc916c4ea05e3da08db
Jackie <jackienorstrom@aol.com>
2019-05-13 08:46:26 -0400

    Berts copy of files.

---
+++ Faxtion Version 1- Single-View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Faxtion Version 1- Single-View
+//
+//  Created by Egbert Sayers on 3/4/19.
+//  Copyright © 2019 Egbert Sayers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Faxtion Version 1- Single-ViewTests/Faxtion_Version_1__Single_ViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  Faxtion_Version_1__Single_ViewTests.swift
+//  Faxtion Version 1- Single-ViewTests
+//
+//  Created by Egbert Sayers on 3/4/19.
+//  Copyright © 2019 Egbert Sayers. All rights reserved.
+//
+
+import XCTest
+@testable import Faxtion_Version_1__Single_View
+
+class Faxtion_Version_1__Single_ViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
