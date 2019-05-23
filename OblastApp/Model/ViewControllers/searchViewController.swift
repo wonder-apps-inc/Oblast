2e9be1fc66a7aa56f274faf33d7bcfefa496dcee
Jackie <jackienorstrom@aol.com>
2019-05-23 13:49:26 -0400

    pushign to remote

--- OblastApp/Model/ViewControllers/searchViewController.swift
+++ OblastApp/Model/ViewControllers/searchViewController.swift
@@ -6,14 +6,26 @@
 //  Copyright © 2019 Egbert Sayers. All rights reserved.
 //
 
+
+/*
+ 
+ Going to get the search bar working in the storyboard first, then change verything over to progmatic code. NO longer using storyboards
+ 
+ 
+ */
+
 import UIKit
 
-class searchViewController: UIViewController, UISearchBarDelegate {
+class searchViewController: UITableViewController, UISearchBarDelegate {
+ 
+    
     
     
     var zipCodesArray = [zipCodes]()
     var filteredZipCodes = [zipCodes]()
     var inSearchMode = false
+    
+//    var zipCodes: zipCodes!
     
     let searchController = UISearchController(searchResultsController: nil)
 
@@ -25,8 +37,11 @@ class searchViewController: UIViewController, UISearchBarDelegate {
 
         // Do any additional setup after loading the view.
         
-        searchBar.returnKeyType = UIReturnKeyType.done
-
+//        searchBar.returnKeyType = UIReturnKeyType.done
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        
         parseZipCodeJSON()
 
         
@@ -77,32 +92,94 @@ class searchViewController: UIViewController, UISearchBarDelegate {
         filteredZipCodes = zipCodesArray.filter({(zipCodes : zipCodes) -> Bool in return zipCodes.townName.lowercased().contains(searchText.lowercased())
             
         })
-//        tableView.reloadData()
+        tableView.reloadData()
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text == nil || searchBar.text == "" {
             inSearchMode = false
             view.endEditing(true)
-//            tableView.reloadData()
+            tableView.reloadData()
         } else {
             inSearchMode = true
             filteredZipCodes = zipCodesArray.filter{$0.townName.range(of: searchBar.text!) != nil}
             
-//            tableView.reloadData()
+            tableView.reloadData()
         }
     }
     
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
-    */
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if inSearchMode {
+            return filteredZipCodes.count
+        }
 
+        
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "zipCode", for: indexPath) as! zipCodeSearchCell
+        //as! SignTableViewCell
+        
+        
+        
+        let zip: zipCodes
+        
+        if inSearchMode {
+            zip = filteredZipCodes[indexPath.row]
+        } else {
+            zip = zipCodesArray[indexPath.row]
+        }
+        //using configure cell instead of this. change back to this if it doesn;t work.
+        //        cell.textLabel!.text = sign.signName
+        //        cell.detailTextLabel!.text = candy.category
+        cell.configureTableCell(zipCode: zip)
+        cell.accessoryType = .detailDisclosureButton
+        
+        return cell
+    }
+    
+
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "signIn", let controller = (segue.destination as? UINavigationController)?.topViewController as? searchViewController {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                
+                let zip: zipCodes
+                if inSearchMode {
+                    zip = filteredZipCodes[indexPath.row]
+                } else {
+                    zip = zipCodesArray[indexPath.row]
+                }
+                //                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                //   let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                
+                //Dictionary button view, is dictionarytableview, the main table view click on is detail view. maybe need another segue with button.
+                
+                
+                controller.zipCodesArray = zip
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+    
+
+
+}
+
+
+extension searchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
 }
